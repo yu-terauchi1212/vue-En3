406befceedae11ac6bb11a07b39517d573f47b9a
寺内 唯 <yu.terauchi@denso-net.local>
2025-06-13 13:02:24 +0900

    ファイル作成

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,16 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/books_img.png" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="読書管理DB" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/input">Input</RouterLink>
+        <RouterLink to="/list">List</RouterLink>
+        <RouterLink to="/updata">Updata</RouterLink>
       </nav>
     </div>
   </header>
--- src/router/index.js
+++ src/router/index.js
@@ -10,13 +10,18 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
+      path: '/input',
+      name: 'input',
+      component: () => import('../views/InputView.vue'),
+    },{
+      path: '/list',
+      name: 'list',
+      component: () => import('../views/ListView.vue'),
+    },{
+      path: '/updata',
+      name: 'updata',
+      component: () => import('../views/UpdataView.vue'),
+    }
   ],
 })
 
