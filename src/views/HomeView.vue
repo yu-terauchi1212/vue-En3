72a4577d5fba9f6cff82f05580482c5495e86fc2
寺内 唯 <yu.terauchi@denso-net.local>
2025-06-13 13:07:49 +0900

    Home作成

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="container">
+      <h1>読書管理DB</h1>
+      <p>このアプリは、読書の記録を管理するためのものです。</p>
+      <p>本の情報を入力し、リストで確認できます。</p>
+    </div>
   </main>
 </template>
